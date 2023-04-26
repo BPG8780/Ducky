05b56ab3f8f7c9f81172853aab69bde941fbb6dd
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-04-27 05:09:18 +0800

    Add files via upload

--- Ducky.sh
+++ Ducky.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-mkdir -p "/root/Ducky/"
-
-# Download DuckyClient function
 function downloadDuckyClient {
     # Check if the script is being run as root
     if [ "$EUID" -ne 0 ]; then
         echo "请以root用户运行此脚本！"
         displayMenu
     fi
+    
+    mkdir -p "/root/Ducky/"
 
-    # Check system architecture and select appropriate DuckyClient version
     if [ $(uname -m) == "x86_64" ]; then
         CPU_ARCH="amd64"
     else
         CPU_ARCH="arm64"
     fi
 
-    # Get latest version number and download URL
     LATEST_VERSION=$(curl --silent https://api.github.com/repos/DuckyProject/DuckyRoBot/releases/latest | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
     DOWNLOAD_URL="https://github.com/DuckyProject/DuckyRoBot/releases/download/$LATEST_VERSION/DuckyClient-$CPU_ARCH"
 
-    # Download DuckyClient from GitHub to /root/Ducky directory
     echo "正在下载DuckyClient $LATEST_VERSION 到 /root/Ducky 目录..."
     wget "$DOWNLOAD_URL" -O "/root/Ducky/DuckyClient" && chmod +x "/root/Ducky/DuckyClient"
 
@@ -29,12 +25,10 @@ function downloadDuckyClient {
     displayMenu
 }
 
-# Create conf.ini file
 function createAndReadConfFile {
     echo "[Client]" > "/root/Ducky/conf.ini"
     read -p "请输入User值和Key值（用空格分隔）：" user_and_key
 
-    # 分离User和Key值
     user_value=$(echo $user_and_key | cut -d' ' -f1)
     key_value=$(echo $user_and_key | cut -d' ' -f2)
 
@@ -45,8 +39,8 @@ function createAndReadConfFile {
     echo "##### Oracle Cloud账户配置 #####"
 
     while true; do
-        read -p "输入 'done' 完成配置添加，或者输入要添加的自定义节名称：" section_name
-        if [[ $section_name == done ]]; then
+        read -p "输入 'BPG' 完成配置、输入要添加的自定义名称：" section_name
+        if [[ $section_name == BPG ]]; then
             break
         fi
         echo "[$section_name]" >> "/root/Ducky/conf.ini"
@@ -61,37 +55,30 @@ function createAndReadConfFile {
 
     echo -e "\033[33mconf.ini文件已创建！\033[0m"
 
-    # Display all configuration sections and options
     awk -F= '/^\[/ {section=$1} /^\[.*$/ {next;} /^user/ {printf("Section: %s, User: %s, Key: %s\n", section, $2, $(getline));} /^fingerprint/ {printf("Fingerprint: %s\n", $2)} /^tenancy/ {printf("Tenancy: %s\n", $2)} /^region/ {printf("Region: %s\n", $2)} /^key_file/ {printf("Key File Path: %s\n\n", $2)}' /root/Ducky/conf.ini
 
-    # Return to menu
     displayMenu
 }
 
-# Read conf.ini file
 function readConfFile {
-    # Check if the conf.ini file exists
     if [ ! -f "/root/Ducky/conf.ini" ]; then
         echo "conf.ini 文件不存在！"
         displayMenu
     fi
 
-    # Display all configuration sections and options
     awk -F= '/^\[/ {section=$1} /^\[.*$/ {next;} /^user/ {printf("Section: %s, User: %s, Key: %s\n", section, $2, $(getline));} /^fingerprint/ {printf("Fingerprint: %s\n", $2)} /^tenancy/ {printf("Tenancy: %s\n", $2)} /^region/ {printf("Region: %s\n", $2)} /^key_file/ {printf("Key File Path: %s\n\n", $2)}' /root/Ducky/conf.ini
 
-    # Return to menu
     displayMenu
 }
 
 function displayMenu {
 echo "请输入要执行的操作："
-echo "1. 下载和安装DuckyClient"
-echo "2. 创建和读取conf.ini文件"
+echo "1. 安装DuckyBot客户端"
+echo "2. 创建conf.ini配置文件"
 echo "3. 读取已有的conf.ini文件"
 echo "4. 退出"
 read -p "请选择： " choice
 
-# Call appropriate function based on user input
 case "$choice" in
     1)
         downloadDuckyClient
