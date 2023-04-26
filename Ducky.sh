404b5e947a7dc15f359bc249ac3822a25a7aa6a2
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-04-26 16:14:04 +0800

    Add files via upload

--- Ducky.sh
+++ Ducky.sh
@@ -9,14 +9,18 @@ fi
 # 输出使用黄色文本的消息
 echo -e "\033[33mThis message is displayed in yellow.\033[0m"
 
+
+CONFIG_FILE="conf.ini"
+REMOTE_CONFIG_URL="https://raw.githubusercontent.com/BPG8780/Ducky/main/conf.ini"
+
 # 定义函数以获取当前操作系统框架类型
 function get_system_architecture() {
   case "$(uname -m)" in
     "x86_64" | "amd64")
       echo "amd64"
       ;;
-    "i386" | "i486" | "i586" | "i686")
-      echo "386"
+    "aarch64" | "arm64")
+      echo "arm64"
       ;;
     *)
       echo "未知的架构类型"
@@ -40,8 +44,22 @@ function get_latest_version() {
 function show_menu() {
   echo "请选择一个选项："
   echo "1. 安装 Ducky Client"
-  echo "2. 选项 2"
+  echo "2. 自定义配置"
   echo "3. 选项 3"
+}
+
+# 定义函数以下载远程配置文件
+function download_config() {
+  curl -L -o "$CONFIG_FILE" "$REMOTE_CONFIG_URL"
+  echo "配置文件已下载。"
+}
+
+# 定义函数以更改配置文件中的值
+function change_config() {
+  read -p "请输入 User 名称：" user_name
+  read -p "请输入 Key 值：" key_value
+  sed -i '' "s/^User=.*/User=$user_name/" "$CONFIG_FILE"
+  sed -i '' "s/^Key=.*/Key=$key_value/" "$CONFIG_FILE"
 }
 
 # 获取用户输入并执行相应操作
@@ -58,7 +76,11 @@ case $choice in
     echo "Ducky Client $latest_version 已安装到 ~/Ducky 目录。"
     ;;
   2)
-    echo "选项 2 已选择"
+    if [ ! -f "$CONFIG_FILE" ]; then
+      download_config
+    fi
+    change_config
+    echo "配置已更改。"
     ;;
   3)
     echo "选项 3 已选择"
