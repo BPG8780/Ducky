c6b19768042519427b46b3c8d18ab58fec847ad9
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-04-26 23:35:14 +0800

    Add files via upload

--- Ducky.sh
+++ Ducky.sh
@@ -31,18 +31,25 @@ function downloadDuckyClient {
 
 # Create conf.ini file
 function createConfFile {
-    echo "[Client]" > /root/Ducky/conf.ini
-    echo "User=" >> /root/Ducky/conf.ini
-    echo "Key=" >> /root/Ducky/conf.ini
-    echo "Port=808" >> /root/Ducky/conf.ini
-    echo "" >> /root/Ducky/conf.ini
-    echo "##### 甲骨文账号配置 #####" >> /root/Ducky/conf.ini
-    echo "[]" >> /root/Ducky/conf.ini
-    echo "user=" >> /root/Ducky/conf.ini
-    echo "fingerprint=" >> /root/Ducky/conf.ini
-    echo "tenancy=" >> /root/Ducky/conf.ini
-    echo "region=" >> /root/Ducky/conf.ini
-    echo "key_file=" >> /root/Ducky/conf.ini
+    echo "[Client]" > "/root/Ducky/conf.ini"
+    read -p "请输入User值: " user
+    echo "User=$user" >> "/root/Ducky/conf.ini"
+    read -p "请输入Key值: " key
+    echo "Key=$key" >> "/root/Ducky/conf.ini"
+    echo "Port=808" >> "/root/Ducky/conf.ini"
+    echo "" >> "/root/Ducky/conf.ini
+    echo "##### 甲骨文账号配置 #####" >> "/root/Ducky/conf.ini"
+    read -p "请输入[]中的user值: " jg_user
+    echo "[]" >> "/root/Ducky/conf.ini"
+    echo "user=$jg_user" >> "/root/Ducky/conf.ini"
+    read -p "请输入[]中的fingerprint值: " fingerprint
+    echo "fingerprint=$fingerprint" >> "/root/Ducky/conf.ini"
+    read -p "请输入[]中的tenancy值: " tenancy
+    echo "tenancy=$tenancy" >> "/root/Ducky/conf.ini"
+    read -p "请输入[]中的region值: " region
+    echo "region=$region" >> "/root/Ducky/conf.ini"
+    read -p "请输入[]中的key_file值: " key_file
+    echo "key_file=$key_file" >> "/root/Ducky/conf.ini"
 
     echo -e "\033[33mconf.ini文件已创建！\033[0m"
 }
@@ -50,7 +57,7 @@ function createConfFile {
 # Display menu
 echo "请选择您要执行的操作："
 echo "1. 下载最新版本的 DuckyClient"
-echo "2. 创建 conf.ini 配置文件"
+echo "2. 创建自定义的 conf.ini 配置文件"
 read choice
 
 # Call appropriate function based on user's choice
@@ -58,7 +65,7 @@ if [ "$choice" == "1" ]; then
     echo -e "\033[33m您选择了下载最新版本的 DuckyClient\033[0m"
     downloadDuckyClient
 elif [ "$choice" == "2" ]; then
-    echo -e "\033[33m您选择了创建 conf.ini 配置文件\033[0m"
+    echo -e "\033[33m您选择了创建自定义的 conf.ini 配置文件\033[0m"
     createConfFile
 else
     echo "尚未实现此功能！"
